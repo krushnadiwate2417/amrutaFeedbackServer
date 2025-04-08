aea7ae90abf58b2891749a8512e3f8fe3aa2b0b1
krushnadiwate2417 <krushnadiwate2417@gmail.com>
2025-04-08 18:32:23 +0530

    initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const routes = require('./src/routes/feedbackRoute');
+const cors = require('cors');
+
+const app = express();
+
+const allowedOrigins = ['http://localhost:5173', 'https://amrutafeedbackcollector.netlify.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+app.use(express.json());
+app.use('/api/v1',routes)
+app.use('/',(req,res,next)=>{
+    res.send({
+        status : "success",
+        error : false
+    })
+    next();
+})
+
+module.exports = app;
